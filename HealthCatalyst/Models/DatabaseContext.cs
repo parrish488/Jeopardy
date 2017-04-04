81d9b8f516f3a01113817e2d07804e2d8e687396
Jason Parrish <jparrish@solutionreach.com>
2017-04-03 21:34:13 -0600

    Get database initializers set up.

---
+++ HealthCatalyst/Models/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HealthCatalyst.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+            : base("DatabaseContext")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ HealthCatalyst/Models/DatabaseInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HealthCatalyst.Models
+{
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            var clients = new List<Client>
+            {
+                new Client {FirstName="John", LastName="Doe", Street="123 First Street", City="Nowhere", State="UT", Zip="84000", PrimaryPhone="(801) 888-8888", Email="john_doe@gmail.com" },
+                new Client {FirstName="Jane", LastName="Doe", Street="123 Second Street", City="Nowhere", State="UT", Zip="84000", PrimaryPhone="(801) 888-8888", Email="jane_doe@gmail.com" }
+            };
+
+            context.Clients.AddRange(clients);
+
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
