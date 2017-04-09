d36038d4f9f5dea1f286b97be2bafcdaa041b24d
Jason Parrish <jparrish@solutionreach.com>
2017-04-08 21:03:26 -0600

    Add pictures and unit tests.

--- HealthCatalyst.Tests/Controllers/SubscribersControllerTest.cs
+++ HealthCatalyst.Tests/Controllers/SubscribersControllerTest.cs
@@ -6,12 +6,22 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthCatalyst;
 using HealthCatalyst.Controllers;
+using System.Data.Entity;
+using HealthCatalyst.Models;
 
 namespace HealthCatalyst.Tests.Controllers
 {
     [TestClass]
     public class SubscribersControllerTest
     {
+        [TestInitialize]
+        public void TestSetup()
+        {
+            DatabaseContext db = new DatabaseContext();
+            db.Subscribers.Add(GetTestSubscriber());
+            db.SaveChanges();
+        }
+
         [TestMethod]
         public void Index()
         {
@@ -23,6 +33,50 @@ namespace HealthCatalyst.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            SubscribersController controller = new SubscribersController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            SubscribersController controller = new SubscribersController();
+
+            // Act
+            ViewResult result = controller.Details(1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            SubscribersController controller = new SubscribersController();
+
+            // Act
+            ViewResult result = controller.Delete(1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        public Subscriber GetTestSubscriber()
+        {
+            return new Subscriber { FirstName = "John", LastName = "Doe", Street = "123 First Street", City = "Nowhere", State = "UT", Zip = "84000", Birthdate = new DateTime(2000, 1, 1), Interests = "Outoors" };
         }
     }
 }
